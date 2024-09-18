b143f4a9ea9f4025bfde385bc22db9290a9e1de7
Sven Schulz <Sven.Schulz@pub.tech>
2024-09-18 15:30:55 +0200

    added viewModel + setup databinding for mainActivity

---
+++ starter/app/src/main/java/com/udacity/shoestore/models/ShoeListViewModel.kt
@@ -0,0 +1,12 @@
+package com.udacity.shoestore.models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ShoeListViewModel: ViewModel() {
+
+    private lateinit var _shoeList: MutableLiveData<MutableList<Shoe>>
+    val shoeList: LiveData<MutableList<Shoe>>
+        get() =_shoeList
+}
